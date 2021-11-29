dbb6a3677ebc7c7614b5e6525f429d4baeef438b
abeige2 <31280510+abeige@users.noreply.github.com>
2021-11-28 21:34:35 -0600

    util

---
+++ Code/util.h
@@ -0,0 +1,26 @@
+void hidecursor() {
+   HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+   CONSOLE_CURSOR_INFO info;
+   info.dwSize = 100;
+   info.bVisible = FALSE;
+   SetConsoleCursorInfo(consoleHandle, &info);
+}
+
+void showcursor() {
+   HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+   CONSOLE_CURSOR_INFO info;
+   info.dwSize = 100;
+   info.bVisible = TRUE;
+   SetConsoleCursorInfo(consoleHandle, &info);
+}
+
+void resize() {
+	HWND console = GetConsoleWindow();
+	RECT ConsoleRect;
+	GetWindowRect(console, &ConsoleRect);
+	MoveWindow(console, ConsoleRect.left, ConsoleRect.top, 600, 600, TRUE);
+}
+
+void clear() {
+    cout << "\x1B[2J\x1B[H";
+}

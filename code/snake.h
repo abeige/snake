f3dbbba030e9effbc5498d3a1379e0dc94bfcea5
abeige <31280510+abeige@users.noreply.github.com>
2021-12-01 15:46:13 -0600

    fixed struct NODE declaration

--- code/snake.h
+++ code/snake.h
@@ -3,22 +3,22 @@
 #include <utility>
 using namespace std;
 
+struct NODE {
+    int x, y;
+    NODE* next;
+
+    NODE(int x, int y) {
+        this->x = x;
+        this->y = y;
+        next = nullptr;
+    }
+};
+
 class Snake {
  private:
 	NODE* head;
 	int length;
     char direction;
-
-    struct NODE {
-        int x, y;
-        NODE* next;
-
-        NODE(int x, int y) {
-            this->x = x;
-            this->y = y;
-            next = nullptr;
-        }
-    };
 
  public:
 	Snake();

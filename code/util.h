6e90beb31af22a8d8be4aeca82588ca0798f2946
abeige2 <abeige2@uic.edu>
2021-12-02 13:18:20 -0600

    added headers and global constants

---
+++ code/util.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define KEY_RETURN 10
+#define NORTH '^'
+#define SOUTH 'v'
+#define EAST  '>'
+#define WEST  '<'
+
+#include <vector>
+#include <string>
+#include <curses.h>
+#include <unistd.h>
